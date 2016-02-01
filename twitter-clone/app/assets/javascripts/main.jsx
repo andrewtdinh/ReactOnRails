27e0ddfa206a35ef78bf71311c6f7f7916792557
Andrew Dinh <andrewdinh74@yahoo.com>
2016-01-31 22:06:19 -0800

    Reverting the component file back to main.jsx

--- twitter-clone/app/assets/javascripts/main.jsx
+++ twitter-clone/app/assets/javascripts/main.jsx
@@ -1,17 +1,17 @@
 class Main extends React.Component {
   render() {
     return (
-      <h1>Important SEO content here</h1>
+      <h1>Hello from the component</h1>
     );
   }
 }
 
 
-// let documentReady = () => {
-//   React.render(
-//     <Main />,
-//     document.getElementById('react')
-//   );
-// };
-//
-// $(documentReady)
+let documentReady = () => {
+  React.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady)
