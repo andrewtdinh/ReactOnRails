8fbae7db789752fae521778367a2a363fd5991d6
Andrew Dinh <andrewdinh74@yahoo.com>
2016-02-02 22:32:01 -0800

    Use Tweet component in TweetList component

--- twitter-clone/app/assets/frontend/components/TweetList.jsx
+++ twitter-clone/app/assets/frontend/components/TweetList.jsx
@@ -1,18 +1,13 @@
+import Tweet from "./Tweet"
+
 export default class TweetList extends React.Component {
   render(){
     return (
       <div>
         <ul className="collection">
-          <li className="collection-item avatar">
-            <i className="material-icons circle">person_pin</i>
-            <span className="title">Andrew Dinh</span>
-            <p>My #FirstTweet</p>
-          </li>
-          <li className="collection-item avatar">
-            <i className="material-icons circle">person_pin</i>
-            <span className="title">Andrew Dinh</span>
-            <p>My #FirstTweet</p>
-          </li>
+          <Tweet />
+          <Tweet />
+          <Tweet />
         </ul>
       </div>
     );
